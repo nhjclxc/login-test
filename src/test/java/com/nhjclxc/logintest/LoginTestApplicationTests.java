7e1ae45a5c73464113fdcc2021d3ce74bad8e062
LuoXianchao <nhjclxc@163.com>
2024-05-10 22:28:19 +0800

    初始化项目

---
+++ src/test/java/com/nhjclxc/logintest/LoginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhjclxc.logintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
